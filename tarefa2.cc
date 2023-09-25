fdf8764e2a20b3724995750e0241fad411a05c41
kevini78 <133727543+kevini78@users.noreply.github.com>
2023-09-25 15:51:02 -0300

    Update tarefa2.cc

--- tarefa2.cc
+++ tarefa2.cc
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double altura_chico = 1.50; // Altura inicial de Chico em metros
-    double altura_ze = 1.10;   // Altura inicial de Zé em metros
-    int anos = 0;             // Número de anos necessários
+    double altura_chico = 1.50; 
+    double altura_ze = 1.10;   
+    int anos = 0;             
 
     while (altura_ze <= altura_chico) {
-        altura_chico += 0.02;  // Chico cresce 2 centímetros por ano (0.02 metros)
-        altura_ze += 0.03;     // Zé cresce 3 centímetros por ano (0.03 metros)
+        altura_chico += 0.02;  
+        altura_ze += 0.03;     
         anos++;
     }
 
